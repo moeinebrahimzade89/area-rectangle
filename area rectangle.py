8a54bfcbc53bbf019db794e57da15e1f72ad0d62
Moein Ebrahimzade <moein.ebrahimzade.89@gmail.com>
2026-07-11 23:02:44 +0330

    Update area rectangle.py

--- area rectangle.py
+++ area rectangle.py
@@ -1,7 +1,7 @@
 try:
-    length = int(input("length:"))
-    width = int(input("width:"))
-    print("area:{}".format(length*width))
+    length = int(input("length: "))
+    width = int(input("width: "))
+    print("area: {}".format(length * width))
 except ValueError:
-    #The bottom line is to simply tell the user if the input is not a number.
+    # The bottom line is to simply tell the user if the input is not a number.
     print("The value is not a number.")
